df215a5a15190ffae2cc8136da1f2072c335e284
dhruvacharya <dhruv.acharya007@gmail.com>
2019-01-20 15:37:24 +0530

    Initial commit

---
+++ src/main/java/com/lelo/ordermicroservice/OrderMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.lelo.ordermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMicroserviceApplication.class, args);
+	}
+
+}
+
